0a2f84f4d0fc91949ba498aee3373ce5432c2487
Rivaldo <ortegaalvarezrivaldo@gmail.com>
2021-12-06 08:59:56 -0500

    agregando el toggle

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,41 +1,73 @@
-import { useSelector, useDispatch } from 'react-redux';
-
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.show);
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    dispatch({ type: "increment" });
   };
   const incrementHandler5 = () => {
-    dispatch({ type: 'increment5' });
+    dispatch({ type: "increment5" });
   };
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    dispatch({ type: "decrement" });
   };
   const decrementHandler5 = () => {
-    dispatch({ type: 'decrement5' });
+    dispatch({ type: "decrement5" });
   };
-  const resetHandler= () => {
-    dispatch({ type: 'reset' });
+  const resetHandler = () => {
+    dispatch({ type: "reset" });
   };
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggleCounter" });
+  };
 
   return (
-    <main className={classes.counter}>
+    <div>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div >
-        <button  className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={resetHandler}>Reset</button>
-        <button  className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={incrementHandler}>Increment</button>
-        <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={decrementHandler}>Decrement</button>
-        <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={incrementHandler5}>Increment5</button>
-        <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={decrementHandler5}>Decrement5</button>
+      {show && <div>{counter}</div>}
+
+      <button
+        className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={resetHandler}
+      >
+        Reset
+      </button>
+      <button
+        className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={incrementHandler}
+      >
+        Increment
+      </button>
+      <button
+        className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={decrementHandler}
+      >
+        Decrement
+      </button>
+      <button
+        className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={incrementHandler5}
+      >
+        Increment5
+      </button>
+      <button
+        className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={decrementHandler5}
+      >
+        Decrement5
+      </button>
+      <div>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          onClick={toggleCounterHandler}
+        >
+          Toggle Counter
+        </button>
       </div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
+    </div>
   );
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,41 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0,show:true }, action) => {
   if (action.type === 'reset') {
     return {
+      ...state,
       counter: state.counter =0,
     };
   }
   if (action.type === 'increment') {
     return {
+      ...state,
       counter: state.counter + 1,
     };
   }
   if (action.type === 'decrement') {
     return {
+      ...state,
       counter: state.counter - 1,
     };
   }
   if (action.type === 'increment5') {
     return {
+      ...state,
       counter: state.counter + 5,
     };
   }
   if (action.type === 'decrement5') {
     return {
+      ...state,
       counter: state.counter - 5,
     };
+  }
+  if(action.type==='toggleCounter'){
+    return {
+      ...state,
+      show: !state.show,
+    }
   }
 
   return state;
